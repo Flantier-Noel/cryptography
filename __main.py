587ac0222afd18a73cbe0971cfbc3bab2c5b1672
Flantier <noel.flantierGH@gmail.com>
2025-12-29 13:56:43 +0000

    modified:   __main.py 	new file:   __pycache__/__main.cpython-312.pyc 	renamed:    test/canvas_create test.py -> tests.py

--- __main.py
+++ __main.py
@@ -9,29 +9,29 @@ class Canvas():
         self.lst_unpack_rep = lst_unpack_rep
 
         length = 0
-        while length < len(lst_unpack_rep) and isinstance(lst_unpack_rep[length], list): 
+        while length < len(lst_unpack_rep) and not(isinstance(lst_unpack_rep[length], list)): 
             length +=1
         
         self.len = length
 
     @classmethod
     def from_text(cls, key_text:str):
-        max_jump = 10
+        max_jump = 9
 
         canvas = []
         key = [len(w) for w in key_text.split(' ')]
 
         rank = 0
         for n in key :
-            for ind in range(rank, rank+n):
-                if ind >= len(canvas) : ind.append([i0+1 for i0 in range(max_jump)])
-            
+            for ind in range(max_jump+1):
+                if rank+ind >= len(canvas) : canvas.append([i0+1 for i0 in range(max_jump)])
+
             possible_values = canvas[rank]
-            value = possible_values[key[n]%len(possible_values)]
+            value = possible_values[n%len(possible_values)]
             canvas[rank] = value
 
             for ind in range(1, value):
-                pop_item(canvas[ind], value-ind)
+                pop_item(canvas[rank+ind], value-ind)
             
             rank += 1
 
@@ -44,4 +44,20 @@ class Canvas():
     def __repr__(self):
         lst_pack = self.pack()
         str_list = [str(n) for n in lst_pack]
-        return '[ '+ ', '.join(str_list) +']'
+        return '['+ ', '.join(str_list) +']'
+    
+## without modular variation :
+from random import randint
+
+def encode_int(cnv, msg_int):
+    code = []
+
+    for ind in range(len(msg_int)) :
+        pos = cnv[ind]
+
+        for _ in range(len(code), pos): code.append(-1)
+
+        if code[ind] == -1 : code[ind] = randint(0, 9)
+        code[pos] = msg_int(pos)-code[ind]
+
+    return code
--- tests.py
+++ tests.py
@@ -3,5 +3,12 @@ from lorem_text import lorem
 import random
 random.text = lorem.words
 
+from __main import*
+
 N = 10
 canvas_key = random.text(N)
+canvas = Canvas.from_text(canvas_key)
+
+print(canvas)
+
+msg_int = [2, 7, 1, 8, 2, 8, 1]

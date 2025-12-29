99a5068e3cb112ac88aedd4df56454c2575c0e91
Flantier <noel.flantierGH@gmail.com>
2025-12-29 14:47:19 +0000

    modified:   __main.py 	modified:   __pycache__/__main.cpython-312.pyc 	modified:   tests.py

--- __main.py
+++ __main.py
@@ -1,11 +1,24 @@
-def pop_item(lst, item):
-    for i in range(len(lst)):
-        if lst[i]==item : 
-            lst.pop(i)
-            break
+import warnings
+
+def pop_item(lst:list, item:object):
+    """ given a list lst and an object item, return a list lst containing elements of lst except item.
+        The object item may not belongs to lst, in this case nothing is done """
+    
+
+    lst_new = []
+
+    for i in range(len(lst)) :
+        if lst[i]==item : lst_new.append(lst[i])
+
+    return lst_new        
 
 class Canvas():
+    """ class of canvas used for the cypher as described in the wiki"""
+
     def __init__(self, lst_unpack_rep=[]):
+        """ lst_unpack_rep:[int] represents represents the whole canvas representation in machine 
+            len:int is the index before which lst_unpack_rep correspond to the canvas usable and after which lst_unpack_rep is purely machine data """
+        
         self.lst_unpack_rep = lst_unpack_rep
 
         length = 0
@@ -14,9 +27,22 @@ class Canvas():
         
         self.len = length
 
+    def __getitem__(self, ind:int):
+        """ return the item of self.lst_unpack_rep at the position ind """
+
+        assert ind > 0, "canvas indices must be positive"
+        assert isinstance(ind, int), "canvas indices must be integer"
+        assert ind < len(self.lst_unpack_rep), "canvas index out of range"
+
+        if ind > self.len : warnings.warn("index given is above user range")
+
+        return self.lst_unpack_rep[ind]
+
     @classmethod
     def from_text(cls, key_text:str):
-        max_jump = 9
+        """ create a canvas from a key text using the method explained in the wiki """
+
+        max_jump = 9 ## could be set differently
 
         canvas = []
         key = [len(w) for w in key_text.split(' ')]
@@ -31,17 +57,21 @@ class Canvas():
             canvas[rank] = value
 
             for ind in range(1, value):
-                pop_item(canvas[rank+ind], value-ind)
+                canvas[rank+ind] = pop_item(canvas[rank+ind], value-ind)
             
             rank += 1
 
         return Canvas(canvas)
 
     def pack(self):
+        """ user friendly method in order to easly get the list representation of the usable part of the canvas """
+        
         L = self.len
         return self.lst_unpack_rep[:L]
     
     def __repr__(self):
+        """ usual special method to represent conveniently the canvas ie with the truncated list built by the pack method"""
+        
         lst_pack = self.pack()
         str_list = [str(n) for n in lst_pack]
         return '['+ ', '.join(str_list) +']'
@@ -49,15 +79,20 @@ class Canvas():
 ## without modular variation :
 from random import randint
 
-def encode_int(cnv, msg_int):
+def encode_int(cnv:Canvas, msg_int:list[int]):
+    """without modular variation -- cypher part which encypher a list of integers msg_int using the canvas cnv as described in the wiki"""
+
     code = []
 
     for ind in range(len(msg_int)) :
-        pos = cnv[ind]
+        pos = ind+cnv[ind]
 
-        for _ in range(len(code), pos): code.append(-1)
+        for _ in range(len(code), pos+1): code.append(None) ## None items are used to filled gaps before they get their values
 
-        if code[ind] == -1 : code[ind] = randint(0, 9)
-        code[pos] = msg_int(pos)-code[ind]
+        if code[ind] == None : code[ind] = randint(0, 9)
+        code[pos] = (msg_int[ind]-code[ind])%25
 
+    for ind in range(len(code)):
+        if code[ind] == None : code[ind] = randint(0, 9) ## gaps may stay at the end of the process
+        
     return code
--- tests.py
+++ tests.py
@@ -5,10 +5,15 @@ random.text = lorem.words
 
 from __main import*
 
-N = 10
+N = 7
 canvas_key = random.text(N)
 canvas = Canvas.from_text(canvas_key)
 
 print(canvas)
 
+
 msg_int = [2, 7, 1, 8, 2, 8, 1]
+cnv = Canvas(lst_unpack_rep=[3,4,2,4,2,4,2])
+code = encode_int(canvas, msg_int)
+
+print(code)
